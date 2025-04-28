f3f7de7852ff99dbf276d704c6bca70e520751df
giuseppecarboneneto <zaperzipor@gmail.com>
2025-04-28 09:16:22 -0300

    Aula 05

--- Aula05/Aula5/src/aula05/Exercicio01.java
+++ Aula05/Aula5/src/aula05/Exercicio01.java
@@ -13,29 +13,29 @@ public class Exercicio01 {
     
     public static void main(String[] args) {
        
-      //Gasto Mensal 1 Janeiro
-Scanner GM1 = new Scanner(System.in);
-System.out.println("Coloque o gasto trimestral : ");
-float gasto_mensal1 = GM1.nextFloat();
+    //Gasto Mensal 1 Janeiro
+    Scanner GM1 = new Scanner(System.in);
+    System.out.println("Coloque o gasto de Janeiro : ");
+    float gasto_mensal1 = GM1.nextFloat();
 
-  //Gasto Mensal 2 Fevereiro
-Scanner GM2 = new Scanner(System.in);
-System.out.println("Coloque o gasto trimestral : ");
-float gasto_mensal2 = GM2.nextFloat();
+    //Gasto Mensal 2 Fevereiro
+    Scanner GM2 = new Scanner(System.in);
+    System.out.println("Coloque o gasto Fevereiro : ");
+    float gasto_mensal2 = GM2.nextFloat();
 
-  //Gasto Mensal 1 Março
-Scanner GM3 = new Scanner(System.in);
-System.out.println("Coloque o gasto trimestral : ");
-float gasto_mensal3 = GM3.nextFloat();
-
-
-//Gasto Trimestral(GT)
-float GT;
-GT = gasto_mensal1+gasto_mensal2+gasto_mensal3;
+    //Gasto Mensal 3 Março
+    Scanner GM3 = new Scanner(System.in);
+    System.out.println("Coloque o gasto Março : ");
+    float gasto_mensal3 = GM3.nextFloat();
 
 
-//Saida de Gasto Trimestral
-        System.out.println("Este é o gasto trimestral: "+GT);
+    //Banco Trimestral(BT)
+    float banco_trimestral;
+    banco_trimestral = gasto_mensal1 + gasto_mensal2 + gasto_mensal3;
+
+
+    //Saida de Gasto Trimestral
+    System.out.println("Este é o gasto trimestral: "+banco_trimestral);
         
         
     }
