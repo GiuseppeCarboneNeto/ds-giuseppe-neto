6d487997e2ded5bae3f386bcc6e90d4ed97a5119
giuseppecarboneneto <zaperzipor@gmail.com>
2025-08-04 09:32:15 -0300

    Aula de Java Main e Usuario

---
+++ Segundo semestre/Aula2.01/main.java
@@ -0,0 +1,27 @@
+public class Main {
+    import java.util.Scanner
+
+    public static void main(String[] args) {
+
+        // Cria um novo objeto Usuario usando o construtor
+        Usuario usuario1 = new Usuario("João", "joao@email.com", 37);
+
+        // Exibe as informações do usuário
+        usuario1.exibirInfo();
+
+        // Altera o nome usando o setter
+        usuario1.setNome("João Siles");
+        usuario1.setIdade(11);
+
+        // Exibe novamente após alteração
+        System.out.println("\nApós alteração de nome:");
+        usuario1.exibirInfo();
+    }
+    }
+
+
+// Criar método para inserir os dados do usuário
+// usando a classe Scanner
+
+ Scanner sc1 = new Scanner(System.in);
+    String textoString = "Maria Silva";
